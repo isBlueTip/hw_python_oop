caf31cebec6f13d42b6c88e693f145715227a24f
Simon Egorov <bluetip@debian-BULLSEYE-live-builder-AMD64>
2021-12-03 11:37:44 +0700

    Training class's methods are defined

--- homework.py
+++ homework.py
@@ -6,23 +6,31 @@ class InfoMessage:
 class Training:
     """Базовый класс тренировки."""
 
+    LEN_STEP = 0.65  # meters in one step
+    M_IN_KM = 1000  # meters in one kilometer
+
     def __init__(self,
                  action: int,
                  duration: float,
                  weight: float,
                  ) -> None:
-        pass
+        self.action = action
+        self.duration = duration
+        self.weight = weight
 
     def get_distance(self) -> float:
         """Получить дистанцию в км."""
-        pass
+
+        return self.action * self.LEN_STEP / self.M_IN_KM
 
     def get_mean_speed(self) -> float:
         """Получить среднюю скорость движения."""
-        pass
+
+        return self.get_distance() / self.duration
 
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
+
         pass
 
     def show_training_info(self) -> InfoMessage:
@@ -42,12 +50,25 @@ class SportsWalking(Training):
 
 class Swimming(Training):
     """Тренировка: плавание."""
+
+    def __init__(self, action: int, duration: float, weight: float, length_pool: int, count_pool: int) -> None:
+        super().__init__(action, duration, weight)
+        self.length_pool = length_pool
+        self.count_pool = count_pool
+
     pass
 
 
 def read_package(workout_type: str, data: list) -> Training:
     """Прочитать данные полученные от датчиков."""
-    pass
+
+    workout_dict = {'SWM': Swimming,
+                    'RUN': Running,
+                    'WLK': SportsWalking}
+    print(workout_dict ['SWM'])
+    session_1 = (workout_dict[workout_type])(data)
+    print(session_1)
+    return session_1
 
 
 def main(training: Training) -> None:
@@ -57,12 +78,12 @@ def main(training: Training) -> None:
 
 if __name__ == '__main__':
     packages = [
-        ('SWM', [720, 1, 80, 25, 40]),
-        ('RUN', [15000, 1, 75]),
-        ('WLK', [9000, 1, 75, 180]),
+        ('SWM', [720, 1, 80, 25, 40]),  # количество гребков, время в часах, вес пользователя, длина бассейна, сколько раз пользователь переплыл бассейн.
+        ('RUN', [15000, 1, 75]),  # количество шагов, время тренировки в часах, вес пользователя.
+        ('WLK', [9000, 1, 75, 180]),  # количество шагов, время тренировки в часах, вес пользователя, рост пользователя.
     ]
 
     for workout_type, data in packages:
         training = read_package(workout_type, data)
-        main(training)
+        # main(training)
 
